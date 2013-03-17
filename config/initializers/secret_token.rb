7d17ae50bbfcf141f1bdce37cda96efcf3836df7
WGS-LAP011 <wgs-lap011@wgslap011.(none)>
2013-03-17 14:20:11 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pamosy::Application.config.secret_token = 'a143650efca47aa697dc891fc70f3d00049692c78a344bed0fafadd9d496bc6209a59e5fa8350e03896c0a2a7d741eea7ec7d669fb5e6cc396c9363c76962424'
